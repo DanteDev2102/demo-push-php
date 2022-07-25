212c5f538949e2ffffe9c4a7dad5115f240cb29f
Jose Acosta <jdap.dmc@gmail.com>
2022-07-25 18:10:17 -0400

    make it better

--- api/subscribe/index.php
+++ api/subscribe/index.php
@@ -1,6 +1,15 @@
 <?php
 
     require_once "../lib/webPush.php";
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+    $method = $_SERVER['REQUEST_METHOD'];
+    if($method == "OPTIONS") {
+       die();
+    }
+
     header('Content-Type: application/json');
 
     try{
